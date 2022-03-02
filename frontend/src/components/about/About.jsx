0bfdcbb5bf4933c5e99399ec1618768779bb62a0
DivineDemon <supame123@gmail.com>
2022-03-02 14:24:17 +0400

    working on About :hammer:

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            
+          </div>
+        </div>
+        <div className="about__content"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
