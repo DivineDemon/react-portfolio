34de4cb1514b16e32b016f6f058b5505d9b09053
DivineDemon <supame123@gmail.com>
2022-03-05 13:47:37 +0400

    Completed About Section :sparkles: :rocket:

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
@@ -9,10 +14,37 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            
+            <img src={ME} alt="about" />
           </div>
         </div>
-        <div className="about__content"></div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi
+            magni provident labore aspernatur dolor odio totam necessitatibus.
+            Impedit quisquam corporis officia illo accusamus est, dignissimos
+            modi autem asperiores architecto.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
       </div>
     </section>
   );
